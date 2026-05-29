76548f4bb75b0974d7bf854e9650861db28f478d
dariasaligina <146203741+dariasaligina@users.noreply.github.com>
2026-05-29 14:27:35 +0300

    ровное отображение названий

--- LanguageLeap/templatetags/filters.py
+++ LanguageLeap/templatetags/filters.py
@@ -1,6 +1,7 @@
 import string
 
 from django import template
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
@@ -8,3 +9,12 @@ register = template.Library()
 @register.filter
 def strip_punctuation(value):
     return value.strip(string.punctuation + '\n\r ')
+
+
+@register.filter
+def set_len(value, arg):
+    if len(value) <= arg:
+        value += ' &nbsp;' * ((arg - len(value)) // 2)
+    else:
+        value = value[:arg - 3] + "..."
+    return mark_safe(value)
